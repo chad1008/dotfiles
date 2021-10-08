f24a1c5464834fba3137a41e9ae5b25791fedf49
chad1008 <13856531+chad1008@users.noreply.github.com>
2021-10-08 10:08:40 -0400

    add christmas countdown function

--- Zsh/functions.sh
+++ Zsh/functions.sh
@@ -13,3 +13,23 @@ pushiterm() {
     git commit
     git push
 }
+
+# Get days until Christmas
+christmas() {
+    YEAR=$(TZ=UTC+4 date -R +"%Y")
+    CHRISTMAS=$(date -jf "%b %d %Z %Y" "Dec 25 EDT $YEAR" "+%s")
+    TODAY=$(date +%s)
+    COUNTDOWN=$(( (${CHRISTMAS} - ${TODAY})/60/60/24 ))
+
+    while getopts ":s" o; do
+        case "${o}" in
+            s)
+                echo "${COUNTDOWN}"
+                ;;
+            *)
+                echo "$(tput bold)$(tput setaf 2)w$(tput setaf 1)u$(tput setaf 2)t$(tput setaf 1)?$(tput sgr0)"
+        esac
+    done
+    shift $((OPTIND-1))
+    if [ $OPTIND -eq 1 ]; then echo "$(tput bold)$(tput setaf 2)${COUNTDOWN} $(tput setaf 1)days $(tput setaf 2)until $(tput setaf 1)christmas$(tput setaf 2)!$(tput setaf 1)!$(tput setaf 2)!$(tput sgr0)"; fi
+}
