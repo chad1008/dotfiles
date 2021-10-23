0b168a32df5b65b70a2a9edab696cf10958ab224
chad1008 <13856531+chad1008@users.noreply.github.com>
2021-10-23 09:18:26 -0400

    upgrade christmas function to handle various possible dates

--- Zsh/functions.sh
+++ Zsh/functions.sh
@@ -16,11 +16,20 @@ pushiterm() {
 
 # Get days until Christmas
 christmas() {
-    YEAR=$(TZ=UTC+4 date -R +"%Y")
-    CHRISTMAS=$(date -jf "%b %d %Z %Y" "Dec 25 EDT $YEAR" "+%s")
+    CHRISTMAS_DAYMONTH="Oct 23"
+    CURRENT_YEAR=$(TZ=UTC+4 date -R +"%Y")
+    CURRENT_CHRISTMAS=$(date -jf "%b %d %Z %Y" "$CHRISTMAS_DAYMONTH EDT $CURRENT_YEAR" "+%s")
     TODAY=$(date +%s)
+    # If Christmas has passed, bump the target date to next year
+    if [[ "${TODAY}" -lt "${CURRENT_CHRISTMAS}" ]]; then
+        COUNTDOWN_YEAR=${CURRENT_YEAR}
+    else
+        COUNTDOWN_YEAR=$(( ${CURRENT_YEAR} + 1 ))
+    fi
+    CHRISTMAS=$(date -jf "%b %d %Z %Y" "$CHRISTMAS_DAYMONTH EDT $COUNTDOWN_YEAR" "+%s")
     COUNTDOWN=$(( (${CHRISTMAS} - ${TODAY})/60/60/24 ))
 
+    # Check for short mode. Output the value or get confused on unrecognized input
     while getopts ":s" o; do
         case "${o}" in
             s)
@@ -31,5 +40,15 @@ christmas() {
         esac
     done
     shift $((OPTIND-1))
-    if [ $OPTIND -eq 1 ]; then echo "$(tput bold)$(tput setaf 2)${COUNTDOWN} $(tput setaf 1)days $(tput setaf 2)until $(tput setaf 1)christmas$(tput setaf 2)!$(tput setaf 1)!$(tput setaf 2)!$(tput sgr0)"; fi
+
+    # If we aren't in short mode, output message for Christmas day, Christmas eve, and the rest of the year
+    if [ $OPTIND -eq 1 ]; then
+        if [[ "${COUNTDOWN}" = 365 ]]; then
+            echo "$(tput bold)$(tput setaf 2)Merry $(tput setaf 1)Christmas$(tput setaf 2)!$(tput setaf 1)!$(tput setaf 2)!$(tput sgr0)"
+        elif [[ "${COUNTDOWN}" = 1 ]]; then
+            echo "$(tput bold)$(tput setaf 2)Tomorrow $(tput setaf 1)is $(tput setaf 2)the $(tput setaf 1)big $(tput setaf 2)day $(tput setaf 1)!$(tput setaf 2)!$(tput setaf 1)!$(tput sgr0)"
+        else 
+            echo "$(tput bold)$(tput setaf 2)${COUNTDOWN} $(tput setaf 1)days $(tput setaf 2)until $(tput setaf 1)christmas$(tput setaf 2)!$(tput setaf 1)!$(tput setaf 2)!$(tput sgr0)"
+        fi
+    fi
 }
