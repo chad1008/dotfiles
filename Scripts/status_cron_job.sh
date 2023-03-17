2fca1b098ca82ae863053be63beec3b4eaaf39e7
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-03-17 11:32:15 -0400

    Update  to accept args for workspace and username

--- Scripts/status_cron_job.sh
+++ Scripts/status_cron_job.sh
@@ -1,10 +1,19 @@
 #!/bin/zsh
 # shellcheck disable=all
+
+# This script is meant to be run as a cron job.
+# It starts by updating all default status that look like christmas countdowns
+# to the correct value.
+# Then it checks the current status for the provided workspace ($1) and
+# username ($2).
+# If the status is empty or has the wrong countdown value, it clears the status,
+# which will trigger the default if it's set in Slackli.
+
 source ~/.zshrc
 countdown_value=$(christmas -s)
 sed -i '' -e "s/\([0-9].*\)\( :christmas_tree:\)/${countdown_value}\2/g" ~/dev/slackli/config.json
 
-current_status=$( slackli home getStatus chad )
+current_status=$( slackli $1 getStatus $2 )
 
 emoji=$(echo $current_status | /opt/homebrew/bin/jq -r '.emoji')
 text=$(echo $current_status | /opt/homebrew/bin/jq -r '.text')
@@ -19,9 +28,7 @@ if ([[ -z $emoji ]] && [[ -z $text ]]) || \
    ([[ $emoji = ":christmas_tree:" ]] && \
    [[ $text =~ "^[0-9]{1,3} :christmas_tree:$" ]] && \
    [[ $text != "${countdown_value} :christmas_tree:" ]]); then
-	echo "updating status..."
-	slackli home status clear
-	slackli a8c status clear
+	slackli $1 status clear
 fi
 
 
