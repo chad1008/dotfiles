e05810470c5019abb611a5a0fb0ecd8cc8cda9e2
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-03-20 07:31:17 -0400

    Add support for different countdowns for specific workspaces

--- Scripts/status_cron_job.sh
+++ Scripts/status_cron_job.sh
@@ -10,23 +10,40 @@
 # which will trigger the default if it's set in Slackli.
 
 source ~/.zshrc
-# countdown_value=$(christmas -s)
-sed -i '' -e "s/\([0-9].*\)\( :christmas_tree:\)/${countdown_value}\2/g" ~/dev/slackli/config.json
 
-current_status=$( slackli $1 getStatus $2 )
+workspace=$1
+username=$2
 
-emoji=$(echo $current_status | /opt/homebrew/bin/jq -r '.emoji')
-text=$(echo $current_status | /opt/homebrew/bin/jq -r '.text')
+# When specifying workspace data, `annual` should be an empty string or `-a`.
+# The latter will pass the proper `annual mode` flag.
+if [[ $workspace = home ]]; then
+   countdown_emoji=":christmas_tree:"
+   countdown_date="Dec 25"
+   annual=''
+elif [[ $workspace = a8c ]]; then
+   countdown_emoji=":colosseum:"
+   countdown_date="Mar 26"
+   annual=''
+fi
+
+countdown_value=$(countdown -s $annual -d "$countdown_date")
+
+sed -i '' -e "s/\([0-9].*\)\( ${countdown_emoji}\)/${countdown_value}\2/g" ~/dev/slackli/config.json
+
+current_status=$( slackli ${workspace} getStatus ${username} )
+
+current_emoji=$(echo $current_status | /opt/homebrew/bin/jq -r '.emoji')
+curent_text=$(echo $current_status | /opt/homebrew/bin/jq -r '.text')
 
 # We update the status if:
 # 1. It's empty OR
-# 2. It has the right emoji AND
-#    It has 1-3 digit number followed by the right emoji AND
+# 2. It has the right current_emoji AND
+#    It has 1-3 digit number followed by the right current_emoji AND
 #    It doesn't have the same number as the countdown value.
 # I'd prefer to combine the last two conditions, but zsh was being a dick.
-if ([[ -z $emoji ]] && [[ -z $text ]]) || \
-   ([[ $emoji = ":christmas_tree:" ]] && \
-   [[ $text =~ "^[0-9]{1,3} :christmas_tree:$" ]] && \
-   [[ $text != "${countdown_value} :christmas_tree:" ]]); then
+if ([[ -z $current_emoji ]] && [[ -z $curent_text ]]) || \
+   ([[ $current_emoji = "${countdown_emoji}" ]] && \
+   [[ $curent_text =~ "^[0-9]{1,3} ${countdown_emoji}$" ]] && \
+   [[ $curent_text != "${countdown_value} ${countdown_emoji}" ]]); then
 	slackli $1 status clear
 fi
