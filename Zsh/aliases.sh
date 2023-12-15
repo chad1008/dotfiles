4af2819ccc3c74bc92022b3def8ae6369f66b87e
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-12-15 10:38:45 -0500

    add nukenberg alias

--- Zsh/aliases.sh
+++ Zsh/aliases.sh
@@ -8,10 +8,10 @@ alias cddf='cd $DOTFILES'
 # Misc
 alias clr='clear'
 alias christmas='countdown -a -d "Dec 25" -n "christmas" -e "Its christmas eve!" -c "Merry Christmas!!! :christmas_tree:"'
+alias nukenberg='npm run wp-env destroy && docker system prune && npm run distclean && npm ci && npx playwright install && npm run build && npm run wp-env start'
 
 # SSH
 alias sshwpcom="ssh -t wpcom 'cd public_html; bash -l'"
 
 # VSCode
-
 alias codein="code-insiders"
